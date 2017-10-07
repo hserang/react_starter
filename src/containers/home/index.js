11b183ee5f3166aeb6f279b86ba886c103ded86e
Haroun Serang <hserang@gmail.com>
2017-10-06 22:21:53 -0700

    Removed counter demo from ui left reducer

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -21,29 +21,7 @@ const Home = props => (
       All ui is scemantic ui for React - just plugged it in for convenience as a temp measure
     </p>
     <p>The jury is out on what if any styling lib to use.</p>
-    <p>Count: {props.count}</p>
 
-    <p>
-      <button onClick={props.increment} disabled={props.isIncrementing}>
-        Increment
-      </button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
-        Increment Async
-      </button>
-    </p>
-
-    <p>
-      <button onClick={props.decrement} disabled={props.isDecrementing}>
-        Decrementing
-      </button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
-        Decrement Async
-      </button>
-    </p>
-
-    <p>
-      <button onClick={() => props.changePage()}>Go to faq via redux</button>
-    </p>
     <DynamicForm {...props.form_data} />
   </div>
 );
